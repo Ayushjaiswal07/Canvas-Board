c35e29939957c11d78eed646d2e19c6298554f63
Ayushjaiswal07 <jaiswalayush029@gmail.com>
2023-11-09 22:11:59 +0530

    Ability to draw is enabled

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -3,8 +3,22 @@ import { useSelector, useDispatch } from 'react-redux';
 
 const Board = () => {
     const canvasRef = useRef(null);
+    const shouldDraw = useRef(false);
     const activeMenuItem = useSelector((state)=> state.menu.activeMenuItem);
     const {color, size} = useSelector((state)=> state.toolbox[activeMenuItem]);
+
+    useEffect(() => {
+        if (!canvasRef.current) return
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d')
+
+        const changeConfig = () => {
+            context.strokeStyle = color
+            context.lineWidth = size
+        }
+
+        changeConfig()
+    }, [color, size])
 
     useEffect(() => {
         if (!canvasRef.current) return
@@ -14,9 +28,37 @@ const Board = () => {
         // when mounting
         canvas.width = window.innerWidth
         canvas.height = window.innerHeight
-    }, [])
 
-    console.log(color, size);
+
+        const handleMouseDown = (e) => {
+            shouldDraw.current = true
+            context.beginPath()
+            context.moveTo(e.clientX, e.clientY)
+        }
+
+        const handleMouseMove = (e) => {
+            if(!shouldDraw.current) return
+            context.lineTo(e.clientX, e.clientY)
+            context.stroke()
+        }
+
+        const handleMouseUp = (e) => {
+            shouldDraw.current = false
+        }
+
+        canvas.addEventListener( 'mousedown', handleMouseDown)
+        canvas.addEventListener( 'mousemove', handleMouseMove)
+        canvas.addEventListener( 'mouseup', handleMouseUp)
+
+        return () => {
+            
+            canvas.removeEventListener( 'mousedown', handleMouseDown)
+            canvas.removeEventListener( 'mousemove', handleMouseMove)
+            canvas.removeEventListener( 'mouseup', handleMouseUp)
+
+        }
+
+    }, [])
 
     return (<canvas ref={canvasRef}></canvas>
     )
